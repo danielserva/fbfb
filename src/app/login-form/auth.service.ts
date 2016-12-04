d227f670f6a5d9c53f63a48ee5003923f4e3ee42
daniel <daniel.serva@gmail.com>
2016-12-04 18:58:41 +0000

    social network authentication via auth0

--- src/app/login-form/auth.service.ts
+++ src/app/login-form/auth.service.ts
@@ -3,22 +3,51 @@ import { tokenNotExpired } from 'angular2-jwt';
 
 // Avoid name not found warnings
 declare var Auth0Lock: any;
+declare var Auth0: any;
 
 @Injectable()
 export class Auth {
   // Configure Auth0
   lock = new Auth0Lock('sGZlFlJ9MXE3ZbOpOC3UlwTDcfZEpuB3', 'danielserva.eu.auth0.com', {});
+  
+  
+  // Configure Auth0
+  // auth0 = new Auth0({
+  //   domain: 'danielserva.eu.auth0.com',
+  //   clientID: 'sGZlFlJ9MXE3ZbOpOC3UlwTDcfZEpuB3',
+  //   responseType: 'token',
+  //   callbackURL: 'http://localhost:4200',
+  // });
+
+  //Store profile object in auth class
+  userProfile: Object;
 
   constructor() {
+
+    // Set userProfile attribute of already saved profile
+    this.userProfile = JSON.parse(localStorage.getItem('profile'));
+
     // Add callback for lock `authenticated` event
     this.lock.on("authenticated", (authResult:any) => {
       localStorage.setItem('id_token', authResult.idToken);
+
+      // Fetch profile information
+      this.lock.getProfile(authResult.idToken, (error: any, profile:any) => {
+        if (error) {
+          // Handle error
+          alert(error);
+          return;
+        }
+        localStorage.setItem('profile', JSON.stringify(profile));
+        this.userProfile = profile;
+      });
     });
   }
 
   public login() {
     // Call the show method to display the widget.
     this.lock.show();
+
   };
 
   public authenticated() {
@@ -30,5 +59,7 @@ export class Auth {
   public logout() {
     // Remove token from localStorage
     localStorage.removeItem('id_token');
+    localStorage.removeItem('profile');
+    this.userProfile = undefined;
   };
 }
